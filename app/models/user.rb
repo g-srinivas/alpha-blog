91298bce361b493f2ffdc10235f4281609c31e8d
Srinivas G <srinu.gandikota@gmail.com>
2025-06-30 12:36:46 +0000

    add password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     has_many :articles
     validates :username, presence: true,
@@ -8,4 +9,6 @@ class User < ApplicationRecord
                                 uniqueness: { case_sensitive: false },
                                 length: { maximum: 105 },
                                 format: { with: VALID_EMAIL_REGEX }
+
+    has_secure_password
 end
