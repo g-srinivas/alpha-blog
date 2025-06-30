1f4cd8733fef20318695340ca33cc721bf06184a
Srinivas G <srinu.gandikota@gmail.com>
2025-06-30 12:22:19 +0000

    display user in ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    VALID_EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     has_many :articles
     validates :username, presence: true,
                                 uniqueness: { case_sensitive: false },
